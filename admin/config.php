7b34d10cb00872013be8d5013a7e1a1c91e5b947
Eyecta Studio <152719844+eyecta@users.noreply.github.com>
2025-07-09 09:48:35 +0200

    Add admin/config.php 1752047315432

--- admin/config.php
+++ admin/config.php
@@ -74,6 +74,11 @@ function getAdminMenuItems() {
             'title' => 'Tendencias',
             'icon' => 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6',
             'url' => '/admin/dashboard.php?section=trending'
+        ],
+        'advertisements' => [
+            'title' => 'Anuncios',
+            'icon' => 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z',
+            'url' => '/admin/dashboard.php?section=advertisements'
         ]
     ];
 }
